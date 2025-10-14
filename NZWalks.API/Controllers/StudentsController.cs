3bf30eb7fc2382ab3c1de27a824554b720c8c253
Kashmitha <kashmithamadushan@gmail.com>
2025-10-14 11:31:29 +0530

    Create Domain Models and Fixed the Git

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentName = new string[] { "John", "Mary", "Peter" };
+
+            return Ok(studentName); // Gives us with 200K success response with data
+        }
+    }
+}
