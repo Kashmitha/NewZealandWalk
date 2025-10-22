1f58a96397ad9e56a0ec8d5d4a3397fff2acb786
Kashmitha <kashmithamadushan@gmail.com>
2025-10-22 12:37:54 +0530

    Add DTos

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using System.Text.RegularExpressions;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GetAll Regions
+        // GET: api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get data from database - domain models
+            var regionsDomain = dbContext.Regions.ToList();
+
+            // Map to DTOs - Data Transfer Objects
+            var regionDto = new List<RegionDto>();
+
+            foreach(var regionDomain in regionsDomain)
+            {
+                regionDto.Add(new RegionDto()
+                {
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+            return Ok(regionDto);
+        }
+
+        // Get Region by Id
+        // GET: api/regions/{id}
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.Find(id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9d6b2c8332b2137840b94b1c46ce95d02d6222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f631d92ac6ccd1235322efcc784a7473d9de5944")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
